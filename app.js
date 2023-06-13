3cf9ee68e1a3fd1a312036238cc155915e1b254e
Jhara02 <jmangondato02@gmail.com>
2023-06-14 01:52:44 +0800

    update app.js views

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const  ejs = require('ejs');
+const path = require('path');
 const mongoose= require("mongoose");
 const _ = require("lodash");
 const PORT=process.env.PORT || 3000;
@@ -9,6 +10,7 @@ const PORT=process.env.PORT || 3000;
 const app = express();
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(bodyParser.urlencoded({extended:true}));
 
